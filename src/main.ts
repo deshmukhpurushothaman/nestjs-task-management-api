04f40432ab67835ad4da4a8d02e36fdb7475bab9
Deshmukh P <deshmukhpurushothaman@gmail.com>
2022-08-09 10:14:05 +0530

    📓Swagger API Doc added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Task Management')
+    .setDescription('API for task management')
+    .setVersion('1.0')
+    .addTag('Tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
